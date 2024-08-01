84b6c61e6c4698e76bc5c2fa6ae525e2117e8894
Akunna Onyekachi <93290304+akunna1@users.noreply.github.com>
2024-08-01 15:27:37 -0400

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,20 @@
+// Navbar.jsx
+import React from 'react';
+import { Link } from 'react-router-dom'; // Ensure you have react-router-dom installed
+
+const Navbar = ({ user }) => {
+  return (
+    <nav className="bg-black p-4 text-white flex justify-between">
+      <div>
+        <h1 className="text-3xl font-bold">YouTube Video Summary Generator</h1>
+      </div>
+      <div>
+        <a href="#" className="text-white hover:underline mx-4">Welcome {user.username}</a>
+        <Link to="/blog-list" className="text-white hover:underline mx-4">Saved Blog Posts</Link>
+        <a href="#" className="text-white hover:underline">Logout</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
